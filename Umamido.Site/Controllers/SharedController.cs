e5dd61add50f21389fff417e51c238239a5a3599
alkonstantinov <alkonstantinov@outlook.com>
2017-08-14 16:20:46 +0300

    Превод на интерфейса

---
+++ Umamido.Site/Controllers/SharedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Umamido.Site.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public ActionResult SetLanguage(string language)
+        {
+            HttpContext.Session["culture"] = language;
+            return Redirect(Request.UrlReferrer.AbsoluteUri);
+
+        }
+    }
+}
---
+++ Umamido.Site/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Umamido.Site
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            HttpContext context = HttpContext.Current;
+
+
+            string language = "en-US";
+            if (context.Session != null && context.Session["culture"] != null)
+                language = context.Session["culture"].ToString();
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(language);
+
+
+        }
+
+        void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
